cf467b70b8105bb48df9d75643b4e3d27e0647eb
RobbinZhu <robbin.zhu@uuabc.com>
2018-12-24 13:02:44 +0800

    tile_component

--- component/tile_component.js
+++ component/tile_component.js
@@ -1,12 +1,158 @@
+import ImageLoader from '../util/image_loader.js'
+import Ajax from '../util/ajax.js'
+import NodeTreeComponent from './node_tree_component.js'
+
 const cache = []
 class TileComponent {
     constructor() {}
     init(scene) {
         this.host = scene
+        this.imageLoaded = false
+        this.nodeTreeComponent = NodeTreeComponent.create(this)
         return this
+    }
+    async load(url) {
+        const json = JSON.parse(await Ajax.get(url))
+        this.json = json
+        this.layers = this.initLayers(json)
+        this.image = null
+        this.imageUrl = json.tilesets && json.tilesets[0] && json.tilesets[0].image
+
+        this.tilewidthI = 1 / json.tilewidth
+        this.tileheightI = 1 / json.tileheight
+        this.width = json.tilewidth * json.width
+        this.height = json.tileheight * json.height
+    }
+    initLayers(tiledMap) {
+        const mapLayers = tiledMap.layers
+        const tileheight = tiledMap.tileheight
+        const tilewidth = tiledMap.tilewidth
+        const layers = []
+        const tileset = tiledMap.tilesets[0]
+        const countX = (tileset.imagewidth / tileset.tilewidth) | 0
+        const countY = (tileset.imageheight / tileset.tileheight) | 0
+        const countXI = 1 / countX
+        const countYI = 1 / countY
+        const image = tileset.image
+        const firstgid = tileset.firstgid
+        const tileoffset = tileset.tileoffset
+        for (let a = 0, b = mapLayers.length; a < b; a++) {
+            const layer = mapLayers[a]
+            const layerwidth = layer.width
+            const layerheight = layer.height
+            if (!layer.visible) {
+                continue
+            }
+            switch (layer.type) {
+                case 'tilelayer':
+                    const items = []
+                    const layerData = {
+                        tilewidth: tilewidth,
+                        tileheight: tileheight,
+
+                        name: layer.name,
+
+                        width: layer.width,
+                        height: layer.height,
+
+                        opacity: layer.opacity,
+                        type: layer.type,
+                        visible: layer.visible,
+
+                        x: layer.x,
+                        y: layer.y,
+
+                        items: items
+                    }
+
+                    layers.push(layerData)
+                    const data = layer.data
+                    let x = 0,
+                        y = 0
+                    for (let i = 0, j = data.length; i < j; i++) {
+                        let gid = data[i]
+                        if (gid > 0) {
+                            gid -= firstgid
+                            items.push([
+                                gid, //x, y,
+                                (gid % countX) * tileset.tilewidth - tileoffset.x, ((gid * countXI) | 0) * tileset.tileheight + tileoffset.y,
+                                tilewidth, tileheight,
+                                x * tilewidth, y * tileheight
+                            ])
+                        } else {
+                            items.push(null)
+                        }
+
+                        if (++x == layerwidth) {
+                            x = 0
+                            y++
+                        }
+                    }
+                    break
+                case 'objectgroup':
+                    for (let i = 0, j = layer.objects.length; i < j; i++) {
+                        const object = layer.objects[i]
+                        const gid = object.gid
+                        if (gid > 0) {
+                            continue
+                        }
+                    }
+                    console.log('objectgroup not support')
+                    break
+            }
+        }
+        return layers
     }
     update() {
         // console.log('TileComponent update')
+    }
+    handleInteractive() {}
+    render(session, camera, alpha) {
+        if (!this.imageUrl) {
+            return
+        }
+        if (!this.image) {
+            if (!this.imageLoaded) {
+                this.imageLoaded = true
+                ImageLoader.load(this.imageUrl).then((image) => {
+                    this.image = image
+                })
+            }
+            return
+        }
+        const layers = this.layers
+        const x1 = camera.frame.x1,
+            y1 = camera.frame.y1,
+            x2 = camera.frame.x2,
+            y2 = camera.frame.y2
+        session.setAlpha(alpha)
+        session.setTransformMatrix(camera._matrixI)
+        for (let i = 0, j = layers.length; i < j; i++) {
+            this.renderLayer(
+                session,
+                layers[i],
+                this.image,
+                (x1 * this.tilewidthI) | 0,
+                Math.ceil(x2 * this.tilewidthI),
+                (y1 * this.tilewidthI) | 0,
+                Math.ceil(y2 * this.tilewidthI)
+            )
+        }
+    }
+    renderLayer(session, layer, texture, xMin, xMax, yMin, yMax) {
+        const items = layer.items
+        for (var i = yMin; i < yMax; i++) {
+            const startIndex = i * layer.width
+            for (var j = xMin; j < xMax; j++) {
+                const cell = items[startIndex + j]
+                if (cell) {
+                    session.drawImage(texture,
+                        cell[1], cell[2], cell[3], cell[4],
+                        cell[5] - 1, cell[6] - 1, cell[3] + 2, cell[4] + 2
+                    )
+                }
+            }
+        }
     }
     remove() {
         this.host = null
--- index.js
+++ index.js
@@ -7,29 +7,72 @@ import ImageLoader from './util/image_loader.js'
 const app = App.create()
 const scene = Scene.create()
 
-const btn = Node.create()
-btn.spaceComponent.relative.update(0.5, 0.5)
-btn.spaceComponent.width =
-    btn.graphicsComponent.width =
-    btn.spaceComponent.height =
-    btn.graphicsComponent.spaceComponent = 100
-btn.graphicsComponent.color = 'blue'
+const btnLeft = Node.create()
+btnLeft.graphicsComponent.notUseCamera = true
+btnLeft.graphicsComponent.color = 'red'
+btnLeft.graphicsComponent.width =
+    btnLeft.spaceComponent.width = 200
+btnLeft.graphicsComponent.height =
+    btnLeft.spaceComponent.height = 100
+btnLeft.spaceComponent.relative.update(0, 1)
+btnLeft.spaceComponent.anchor.update(0, 1)
+btnLeft.addHookSupport()
 
-btn.addHookSupport()
-btn.hookComponent.onUpdate.push(function(session) {
+btnLeft.hookComponent.onUpdate.push(function(session) {
     const inputId = session.getCommandInput(this.inputId)
     this.graphicsComponent.color = inputId ? 'red' : 'blue'
 })
 
-btn.setInteractive(true)
-btn.inputId = 1
-btn.keyCode = 'D'.charCodeAt(0)
+btnLeft.setInteractive(true)
+btnLeft.inputId = 1
+btnLeft.keyCode = 'A'.charCodeAt(0)
 
-scene.nodeTreeComponent.addChild(btn)
+scene.nodeTreeComponent.addChild(btnLeft)
+
+const btnRight = Node.create()
+btnRight.graphicsComponent.notUseCamera = true
+btnRight.graphicsComponent.color = 'red'
+btnRight.graphicsComponent.width =
+    btnRight.spaceComponent.width = 200
+btnRight.graphicsComponent.height =
+    btnRight.spaceComponent.height = 100
+btnRight.spaceComponent.relative.update(1, 1)
+btnRight.spaceComponent.anchor.update(1, 1)
+btnRight.addHookSupport()
+
+btnRight.hookComponent.onUpdate.push(function(session) {
+    const inputId = session.getCommandInput(this.inputId)
+    this.graphicsComponent.color = inputId ? 'red' : 'blue'
+})
+
+btnRight.setInteractive(true)
+btnRight.inputId = 2
+btnRight.keyCode = 'D'.charCodeAt(0)
+
+scene.nodeTreeComponent.addChild(btnRight)
+
+scene.addTileSupport()
+
+scene.tileComponent.load('./resource/tiled.json')
+scene.tileComponent.nodeTreeComponent.setZIndex(-1)
+scene.nodeTreeComponent.sortChildren()
+scene.addCameraSupport()
+
+scene.cameraComponent.position.update(100, 1500)
+scene.cameraComponent.addHookSupport()
+scene.cameraComponent.hookComponent.onUpdate.push(function(session) {
+    if (session.getCommandInput(btnLeft.inputId)) {
+        this.position.x -= 5
+    }
+    if (session.getCommandInput(btnRight.inputId)) {
+        this.position.x += 5
+    }
+    this.position.x = Math.max(session.getDesignInfo().resolution.x >> 1, this.position.x)
+})
 
 app.sessionComponent.domEventComponent.addTouchInputSupport()
 app.sessionComponent.domEventComponent.addKeyboardInputSupport()
-app.sessionComponent.domEventComponent.listenToKeyCodes([btn.keyCode])
+app.sessionComponent.domEventComponent.listenToKeyCodes([btnLeft.keyCode, btnRight.keyCode])
 
 app.sessionComponent.setDesignSize(1136, 768, 1600, 900)
 
